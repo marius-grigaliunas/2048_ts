9ff25e1e9a3fdc8137d89be8ff28dcf34127ae1d
Marius Grigaliunas <cia.marius.grigaliunas@gmail.com>
2024-01-23 11:06:54 +0100

    added the base files for the game

---
+++ prod/index.js
@@ -0,0 +1,21 @@
+"use strict";
+const container = document.querySelector(".game-container");
+let gridSize = { rows: 4, cols: 4 };
+window.addEventListener("keydown", (event) => {
+    switch (event.key) {
+        case "ArrowUp":
+            console.log(event.key);
+            break;
+        case "ArrowDown":
+            console.log(event.key);
+            break;
+        case "ArrowLeft":
+            console.log(event.key);
+            break;
+        case "ArrowRight":
+            console.log(event.key);
+            break;
+        default:
+            break;
+    }
+});
